9e8b27208528f2f220230021bc9580cb2a2868e0
212637753 <danieladalaid.carranza@ge.com>
2018-06-22 13:56:51 -0500

    is lucky

---
+++ code-fights/smooth-sailing/isLucky.py
@@ -0,0 +1,16 @@
+def isLucky(n):
+    number = str(n)
+    length = int(len(number)/2)
+    first = number[0:length]
+    second = number[length:len(number)]
+    firstHalf = 0
+    secondHalf = 0
+    for i in range(len(first)):
+        firstHalf += int(first[i])
+        secondHalf += int(second[i])
+    if firstHalf == secondHalf:
+        return True
+    return False
+
+if __name__ == '__main__':
+    print(isLucky(1230))
