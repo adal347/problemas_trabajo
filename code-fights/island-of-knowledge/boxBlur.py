15176b18c3e390864818aaacefae65282d97a5c2
212637753 <danieladalaid.carranza@ge.com>
2018-07-06 10:48:47 -0500

    box blur

--- code-fights/island-of-knowledge/boxBlur.py
+++ code-fights/island-of-knowledge/boxBlur.py
@@ -1,1 +1,17 @@
+def imageFilter(image, row, column):
+    sum = 0
+    for i in range(-1, 2):
+        for j in range(-1, 2):
+            sum += image[row + i][column + j]
+    return int(sum / 9)
+
 def boxBlur(image):
+    newImage = []
+    height = len(image)
+    width = len(image[0])
+    for row in range(1, height - 1):
+        newRow = []
+        for column in range(1, width - 1):
+            newRow.append(imageFilter(image, row, column))
+        newImage.append(newRow)
+    return newImage
