e3e5d9daf6d038ce38d632d695bb474a78e288e8
Daniel Adalaid Carranza Alvarez <dadalaid@wssgroup.com>
2018-08-30 14:59:03 -0500

    Tennis set

---
+++ code-fights/arcade/3-The-Core/2-all-the-crossroads/tennisSet.java
@@ -0,0 +1,15 @@
+boolean tennisSet(int score1, int score2) {
+  if (score1 > 7 || score2 > 7) {
+    return false;
+  }
+  if (score1 == score2) {
+    return false;
+  }
+  if ((score1 == 6 && score2 < 5) || (score2 == 6 && score1 < 5)) {
+    return true;
+  }
+  if ((score1 == 7 && score2 >= 5) || (score2 == 7 && score1 >= 5)) {
+    return true;
+  }
+  return false;
+}
