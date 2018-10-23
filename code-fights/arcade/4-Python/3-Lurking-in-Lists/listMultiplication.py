d56fc1386aefc4c697920fba996b5ef81d96e6ea
Daniel Adalaid Carranza Alvarez <dadalaid@wssgroup.com>
2018-10-23 10:14:03 -0500

    19-oct - List Multiplication

---
+++ code-fights/arcade/4-Python/3-Lurking-in-Lists/listMultiplication.py
@@ -0,0 +1,12 @@
+"""
+Assume that you come across the following piece of code:
+
+c = a * b
+
+Can it stand for:
+
+list a repeated b times;
+list b repeated a times?
+
+Answer --> Yes to both options.
+"""
