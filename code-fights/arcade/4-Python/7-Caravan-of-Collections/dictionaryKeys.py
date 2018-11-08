d8976898f57f85a109739e16c2f24d1233f1c03e
adal347 <A01270898@itesm.mx>
2018-11-08 10:35:21 -0600

    7-nov - Dictionary Keys

---
+++ code-fights/arcade/4-Python/7-Caravan-of-Collections/dictionaryKeys.py
@@ -0,0 +1,18 @@
+"""
+Consider the following objects:
+
+set
+frozenset
+tuple of mutable objects
+tuple of immutable objects
+list of mutable objects
+list of immutable objects
+Which of them can be used as dictionary keys?
+"""
+
+"""
+Response:
+
+Objects 2 and 4.
+
+"""
