90e519b506e4298a6edfc73fc5458f1e842bedd0
unknown <adita0898@gmail.com>
2018-12-31 10:29:49 -0600

    31-dic - Books Catalogs

---
+++ code-fights/arcade/2-databases/6-specialties/booksCatalogs.sql
@@ -0,0 +1,5 @@
+/*Please add ; after each select statement*/
+CREATE PROCEDURE booksCatalogs()
+BEGIN
+	SELECT DISTINCT ExtractValue(xml_doc, '/descendant-or-self::author[1]') AS author FROM catalogs ORDER BY Author;
+END
