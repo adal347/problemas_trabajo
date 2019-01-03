ea10892befc01a2bb81bbc0193ad78401ddf8747
unknown <adita0898@gmail.com>
2019-01-03 08:52:17 -0600

    1-ene - Habitat Area

---
+++ code-fights/arcade/2-databases/6-specialties/habitatArea.sql
@@ -0,0 +1,8 @@
+/*Please add ; after each select statement*/
+CREATE PROCEDURE habitatArea()
+BEGIN
+	SET @points = CONCAT('MULTIPOINT(', (SELECT GROUP_CONCAT(CONCAT(x, ' ', y) SEPARATOR ',') FROM places), ')');
+    SET @points = REPLACE(@points, ';', ',');
+
+    SELECT ST_AREA(ST_ConvexHull(ST_GeomFromText(@points))) AS area;
+END
