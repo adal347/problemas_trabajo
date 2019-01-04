d22f765625336c142dcd8067632592b009124205
unknown <adita0898@gmail.com>
2019-01-04 11:41:46 -0600

    add new testcase for is cool team

--- code-fights/arcade/4-Python/12-Showing-Class/isCoolTeam.py
+++ code-fights/arcade/4-Python/12-Showing-Class/isCoolTeam.py
@@ -3,12 +3,49 @@ class Team(object):
         self.names = names
 
     def __bool__(self):
-        from collections import Counter
-        x = Counter(''.join([i[0].lower() for i in self.names]))
-        y = Counter(''.join([i[-1].lower() for i in self.names]))
-        z = (x - y) + (y-x)
-        v = sum(z.values())
-        return v == 2 or v == 0
+        self.names = [i.lower() for i in self.names]
+        letters = "abcdefghijklmnopqrstuvwxyz"
+        FROM  = {i: [] for i in letters}
+        TO    = {i: [] for i in letters}
+
+        for i in letters:
+            for j in self.names:
+                if i == j[-1]:
+                    FROM[i].append(j)
+                if i == j[0]:
+                    TO[i].append(j)
+
+        start = False
+        end   = False
+
+        for i in letters:
+            if len(FROM[i]) + 1 == len(TO[i]):
+                if start:
+                    return False
+                start = True
+            elif len(FROM[i]) == len(TO[i]) + 1:
+                if end:
+                    return False
+                end = True
+            elif len(FROM[i]) != len(TO[i]):
+                return False
+
+        visited = set()
+        queue = [0]
+
+        while queue:
+            name = queue.pop(0)
+            visited.add(name)
+
+            for ind, teammate in enumerate(self.names):
+                if ind in visited:
+                    continue
+                if self.names[name][0] == teammate[-1]:
+                    queue.append(ind)
+                elif self.names[name][-1] == teammate[0]:
+                    queue.append(ind)
+
+        return len(visited) == len(self.names)
 
 def isCoolTeam(team):
     return bool(Team(team))
