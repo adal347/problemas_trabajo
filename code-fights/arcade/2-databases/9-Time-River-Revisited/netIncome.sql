909b308fc000a0ef90d937a73c46741ce80d3847
Daniel Adalaid Carranza Alvarez <adita0898@gmail.com>
2019-01-11 10:52:34 -0600

    11-ene - Net Income

---
+++ code-fights/arcade/2-databases/9-Time-River-Revisited/netIncome.sql
@@ -0,0 +1,8 @@
+/*Please add ; after each select statement*/
+CREATE PROCEDURE netIncome()
+BEGIN
+	SELECT year(date) AS year, quarter(date) AS quarter,
+          SUM(profit) - SUM(loss) AS net_profit
+  FROM accounting
+  GROUP BY 1, 2;
+END
