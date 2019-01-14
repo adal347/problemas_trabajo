59f5aa6406267876b90801345d25913e179e1eb3
Daniel Adalaid Carranza Alvarez <adita0898@gmail.com>
2019-01-14 11:23:40 -0600

    14-ene - Scholarships Distribution

---
+++ code-fights/arcade/2-databases/10-JOIN-Us-at-the-Table/scholarshipsDistribution.sql
@@ -0,0 +1,6 @@
+/*Please add ; after each select statement*/
+CREATE PROCEDURE scholarshipsDistribution()
+BEGIN
+	SELECT candidate_id AS student_id FROM candidates LEFT JOIN detentions
+    ON candidate_id = student_id WHERE student_id IS NULL;
+END
