08852f34a5a4b239785070a879d23e29f5c23c02
L03126257 <L03126257@L03126257L01>
2019-03-05 10:37:07 -0600

    28 - ene - Reverse In Parentheses

--- code-fights/arcade/1-intro/3-smooth-sailing/reverseParentheses.py
+++ code-fights/arcade/1-intro/3-smooth-sailing/reverseParentheses.py
@@ -1,21 +1,2 @@
-def doReverse(i, s):
-    j = s.find(")")
-    reverse = s[i:j]
-    s = s[:j] + "" + s[j + 1:]
-    s = s[:i] + reverse[::-1] + s[j:]
-    return s
-
-def reverseParentheses(s):
-    i = s.find("(")
-    if i != -1:
-        s = s[:i] + "" + s[i + 1:]
-        x = s.find("(")
-        y = s.find(")")
-        if x != -1 and x < y:
-            s = reverseParentheses(s)
-        elif x != -1 and x > y:
-            s = doReverse(i, s)
-            s = reverseParentheses(s)
-            return s
-        s = doReverse(i, s)
-    return s
+def reverseInParentheses(s):
+    return eval('"' + s.replace('(', '"+("').replace(')', '")[::-1]+"') + '"')
