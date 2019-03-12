9f3d8c4c39262d7ceb551cfdf4492e164f0bed1c
L03126257 <L03126257@L03126257L01>
2019-03-12 11:56:12 -0600

    10 - feb - Customer Messages

---
+++ code-fights/arcade/2-databases/13-Express-your-CREATivity/customerMessages.sql
@@ -0,0 +1,16 @@
+DROP FUNCTION IF EXISTS response;
+CREATE FUNCTION response(name VARCHAR(40)) RETURNS VARCHAR(200) DETERMINISTIC
+BEGIN
+    RETURN CONCAT("Dear ", UCASE(LEFT(name, 1)),
+                  LOWER(SUBSTRING(SUBSTRING_INDEX(name, ' ', 1), 2)), " ",
+                  UCASE(LEFT(SUBSTRING_INDEX(name, ' ', -1), 1)),
+                  LOWER(SUBSTRING(SUBSTRING_INDEX(name, ' ', -1), 2)),
+                  "! We received your message and will process it as soon as possible.",
+                  " Thanks for using our service. FooBar On! - FooBarIO team.");
+END;
+
+CREATE PROCEDURE customerMessages()
+BEGIN
+    SELECT id, name, response(name) AS response
+    FROM customers;
+END;
